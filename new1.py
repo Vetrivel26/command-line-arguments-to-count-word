4d6e8b456a7116023156a25a39c9b8d9442add1e
Vetrivel AIML <95363138+Vetrivel26@users.noreply.github.com>
2022-02-12 10:46:35 +0530

    Add files via upload

---
+++ new1.py
@@ -0,0 +1,9 @@
+#name:VETRIVEL S
+#ref no:21000733
+import sys
+count=0
+with open(sys.argv[1],'r') as f:
+    for line in f:
+        word = line.split()
+        count += len(word)
+print("Word Count in File = ",count)
